668f26d6c84e98743b3557e82d84d37f8514dc10
brunapatricia <bpnerd83@gmail.com>
2022-01-16 11:02:46 -0300

    Alterar status para Está vivo?

--- script.js
+++ script.js
@@ -3,3 +3,22 @@ const botao =document.querySelector('button');
 const nomeDoPersonagem =document.querySelector('#nome');
 const especie =Document.querySelector('#especie');
 const status =document.querySelector('#status');
+
+
+pegarPersonagem =() => {
+    return fetch(`https://rickandmortyapi.com/api/character/1,2,3`,{
+     method: 'GET',
+      headers: {
+          Acept: 'application/jason',
+          "Content-type": 'application/jason'
+      }
+    }).then((Response => response.jason().then((data) => {
+        imagem.src= data.image;
+        imagem.alt= data.name;
+        nomeDoPersonagem.innerHTML= data.nome;
+    });
+
+}
+
+botao.onclick =pegarPersonagem;
+
